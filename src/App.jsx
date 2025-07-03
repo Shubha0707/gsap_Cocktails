0d14990d606f215856c35dd03be7b890cf77832d
Shubhadeep Bhowmik <shubhadeepisok07@gmail.com>
2025-07-03 22:03:15 +0530

    Implement the Menu section and animation

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Hero from './Components/Hero';
 import Cocktails from './Components/Cocktails';
 import About from './Components/About';
 import Art from './Components/Art';
+import Menu from './Components/Menu';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -17,6 +18,7 @@ const App = () => {
       <Cocktails />
       <About />
       <Art />
+      <Menu />
     </main>
   )
 }
